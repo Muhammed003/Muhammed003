098ea6e6668350fbbb7eae08c4747ad4bbead131
MUKHAMED <57451048+Muhammed003@users.noreply.github.com>
2022-04-20 10:10:34 +0600

    Create file.py

---
+++ file.py
@@ -0,0 +1,34 @@
+# ! Task 1
+# with open('task1.txt', 'r+') as file:
+# 	for i in file.readlines(9):
+# 		print(i)
+   #  data = file.readlines(5)
+
+# ! Task 2
+
+# with open("task2.txt") as file:
+#     for line in file:
+#         print(line)
+
+# ! Task 3
+# with open("numbers.txt", "w+") as file:
+#     for number in range(0, 10):
+#         file.write(str(number) + '*')
+# with open("numbers.txt", "r") as file1:
+# 	print(file1.read())
+# ! Task 4
+# i = 0
+# with open("task4.txt", "rt") as file:
+# 	for line in file.readlines():
+# 		i = i+1
+# 	print(i)
+# ! Task 5
+# with open('numbers.txt', 'r') as file:
+#     list_ = list(map(int, file.readlines()))
+#     with open('python19.txt', 'w') as file2:
+#         file2.write(str(min(list_))+'-' + str(max(list_)))
+#     with open('python19.txt', 'r') as file3:
+#         print(file3.read())
+
+
+# ! Code.py
