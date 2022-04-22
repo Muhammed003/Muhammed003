5a27b3abf194efed2aa734776cba457e5b05e65b
MUKHAMED <57451048+Muhammed003@users.noreply.github.com>
2022-04-22 17:51:30 +0600

    Create parsing.py

---
+++ parsing.py
@@ -0,0 +1,88 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+URL = "https://kg.wildberries.ru/catalog/elektronika/igry-i-razvlecheniya/aksessuary/garnitury"
+def get_html(url: str): 
+	html = requests.get(url=URL)
+	return html.text
+
+def get_last_page(html):
+	soup = BeautifulSoup(html, 'lxml')
+	pagination = soup.find('div', class_="pager i-pager pagination").find_all("a", class_="pagination-item pagination__item")[-1].text
+	return int(pagination)
+
+
+def write_to_csv(data: list) -> None:
+	"""Запись в csv формате"""
+	with open("data/info.csv", "w") as file:
+		writer = csv.writer(file)
+		writer.writerow(["name", "price", "description", "image"])
+		for item in data:
+			writer.writerow([item.get("name"), 
+								  item.get("price"),
+								  item.get("description"),
+								  item.get("image")]
+								  )		
+			# download_image(url=item.get("image"), 
+			# 					name=item.get("name"))
+
+
+DATA = []
+def get_description(url, name, price):
+	URL = url
+	html2 = requests.get(URL).text
+	soup2 = BeautifulSoup(html2, 'lxml')
+	try:
+		description = soup2.find("div", class_="collapsable__content j-description").text
+	except:
+		description = ''
+	try:
+		image = soup2.find('img', class_="photo-zoom__preview j-zoom-preview").get('src')
+		image = "https:"+image
+	except:
+		image = ''
+	dep = {"name": name, 
+		"price": price, 
+		"description": description, 
+		"image": image
+		} 
+	DATA.append(dep)
+
+# def download_image(url: str, name: str) -> None:
+# 	response = requests.get(url = url)
+# 	with open(f"data/image/{name}.jpg", "wb") as file:
+# 		file.write(response.content)
+
+
+def get_data(html):
+	soup = BeautifulSoup(html, 'lxml')
+	all_things = soup.find("div", class_="product-card-list").find_all('div', class_="product-card j-card-item")
+	for cards in all_things:
+		try:
+			price = cards.find('div', class_="product-card__price j-cataloger-price").find('ins', class_='lower-price').text
+			# print(price)
+		except:
+			price = ''
+		try:
+			name = cards.find('div', class_="product-card__brand-name").text
+			# print(name)
+		except:
+			name = ''
+		try:
+			link = cards.find('div', class_='product-card__wrapper').find('a', class_='product-card__main').get('href')
+			link = "https://kg.wildberries.ru" + link
+		except:
+			link = ''
+		
+		
+		get_description(link, name, price)
+		
+def main():
+	html = get_html(url = URL)
+	for i in range(1, get_last_page(html)+1):
+		URL_PAGE = URL +"?sort=popular&page=" + str(i)
+		html = get_html(url = URL_PAGE)
+		get_data(html=html)
+	write_to_csv(DATA)
+main()
